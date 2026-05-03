af71f4c99de8125ee1a72ad1426414b2e9a92ebc
dev-marche-it <alemarchesi123@gmail.com>
2026-05-03 12:30:55 +0200

    feat(web): Flask setup halfWay completed

--- web/config.py
+++ web/config.py
@@ -1,5 +1,4 @@
 import os
-
 # Directory radice del repository (genitore della cartella web/)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, "data")
